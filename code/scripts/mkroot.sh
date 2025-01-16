82b646f3414263ccddfcfeb8797dad666fb86b60
ZuxiongPan <pzxiong9865@gmail.com>
2025-01-16 22:04:53 +0800

    modify code arch

--- code/scripts/mkroot.sh
+++ code/scripts/mkroot.sh
@@ -5,7 +5,7 @@ echo "----- make rootfs -----"
 cp $COMPILER_DIR/arm-linux-gnueabihf/libc/lib/ld-linux-armhf.so.3 $ROOT_DIR/lib/ -f
 cp $COMPILER_DIR/arm-linux-gnueabihf/libc/lib/libc.so.6 $ROOT_DIR/lib/ -f
 find $KERNEL_DIR/drivers -name "*.ko" -exec cp -f {} $ROOT_DIR/modules/ \;
-find $CODE_DIR/drivers -name "*.ko" -exec cp -f {} $ROOT_DIR/modules/ \;
+find $CUSKERNEL_DIR/drivers -name "*.ko" -exec cp -f {} $ROOT_DIR/modules/ \;
 
 # filesystem blocksize 4096Bytes, block numbers 0x2000
 mkfs.ext4 -F -b 4096 -d $ROOT_DIR $TARGET_DIR/sdcard 0x2000
